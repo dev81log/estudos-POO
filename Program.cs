f22dca7ba7cff5840d61b1d7ab4551ce33af1f95
dev81log <dev81log@gmail.com>
2022-06-06 20:19:57 -0300

    atualização de encapsulamento

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Pagamentos
 {
@@ -6,22 +7,49 @@ namespace Pagamentos
     {
         static void Main()
         {
-            var PagamentoBoleto = new PagamentoBoleto();
-            PagamentoBoleto.vencimento = DateTime.Now;
-            PagamentoBoleto.numeroBoleto = "22334455";
-
             Console.Clear();
-            PagamentoBoleto.Pagar();
+
+            var Pagamento = new Pagamento(); // instanciando a classe de Pagamento
+            Pagamento.DataPagamento = DateTime.Now; // atribuindo propriedade
+            var Endereco = new Endereco(); // instanciando a classe de Endereco
+            var PagamentoBoleto = new PagamentoBoleto(); // instanciando a classe de PagamentoBoleto (recebendo herança de Pagamento)
+            PagamentoBoleto.Pagar(); // chamando metodo da classe de PagamentoBoleto (recebendo metodo Pagar)
+            PagamentoBoleto.vencimento = DateTime.Now; // atribuindo propriedade 
+            PagamentoBoleto.numeroBoleto = "22334455"; // atribuindo propriedade 
+
+            Console.WriteLine("Endereco: " + Endereco.cep + " " + Endereco.cidade);
             Console.WriteLine("Pagamento Boleto: " + PagamentoBoleto.vencimento.ToString());
             Console.WriteLine("Numero Boleto: " + PagamentoBoleto.numeroBoleto);
+
         }
     }
 
+    // classe
     class Pagamento
     {
+        // prop simples
+        public DateTime Vencimento { get; set; }
+
+        // prop full
+        private DateTime _dataPagamento; // _ propriedade privado
+
+        public DateTime DataPagamento
+        {
+            get
+            {
+                return _dataPagamento;
+                Console.WriteLine("Lendo Pagamento");
+            }
+            set
+            {
+                _dataPagamento = value;
+                Console.WriteLine("Atribuindo Pagamento");
+            }
+        }
+
+
         // propriedades
         public DateTime vencimento;
-        public Endereco endereco;
 
         // metodos
         public void Pagar()
@@ -31,6 +59,7 @@ namespace Pagamentos
 
     }
 
+    // herança
     class PagamentoBoleto : Pagamento
     {
         public string numeroBoleto;
@@ -43,7 +72,8 @@ namespace Pagamentos
 
     public class Endereco
     {
-        string cep;
+        public string cep = "89022-0222";
+        public string cidade = "nova cidade";
     }
 }
 
