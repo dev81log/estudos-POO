81e9f2343254440e05d7f64fde39c6ccecd7e6f1
dev81log <dev81log@gmail.com>
2022-06-07 22:12:58 -0300

    Estudo do parametro Base

--- Metodos/Program.cs
+++ Metodos/Program.cs
@@ -6,13 +6,21 @@ namespace Metodos
     {
         static void Main(string[] args)
         {
-            var sobrecargaExemplo = new MetodosSobrecargaDois();
+            var sobrecargaExemplo = new MetodosSobrecargaDois(DateTime.Now);
             sobrecargaExemplo.Sobrecarga("string");
         }
     }
 
     public class MetodosSobrecarga
     {
+        public DateTime HoraSobrecarga { get; set; }
+
+        public MetodosSobrecarga(DateTime horaSobrecarga)
+        {
+            horaSobrecarga = DateTime.Now;
+            Console.WriteLine("Inicando Metodo Construtor");
+        }
+
         public virtual void Sobrecarga(string argument)
         {
             Console.WriteLine("sobrecarga 1 efetuado");
@@ -21,6 +29,8 @@ namespace Metodos
 
     public class MetodosSobrecargaDois : MetodosSobrecarga
     {
+        public MetodosSobrecargaDois(DateTime horaSobrecarga) : base(horaSobrecarga) { }
+
         public override void Sobrecarga(string argument)
         {
             base.Sobrecarga(argument);
