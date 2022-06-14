34c7b06f14e9461b479f164f0f5cb0e249ebdd40
dev81log <dev81log@gmail.com>
2022-06-13 21:10:36 -0300

    Classes Abstratas com virtual e override

--- ClassesAbstratas/Program.cs
+++ ClassesAbstratas/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public abstract class Payment : IPaymentManager // Classe abstrata(não pode ser instanciando) Herança do contrato a ser seguido
+        {
+            public virtual DateTime GetDateTime { get; set; } // variaveis sendo virtual 
+            public virtual string GetZipcode { get; set; }
+            public virtual void GetPaymentDetails(double value)
+            {
+
+            }
+        }
+
+        public class PaymentCrediCard : Payment // herança de Payment 
+        {
+            public override void GetPaymentDetails(double value) // variaveis sobrescrevendo virtual
+            {
+                base.GetPaymentDetails(value);
+            }
+        }
+
+        public class PaymentApplePayment : Payment // Herança de Payment
+        {
+            public override void GetPaymentDetails(double value)
+            {
+                base.GetPaymentDetails(value);
+            }
+        }
+
+
+        public interface IPaymentManager // 'I' para implementar interface ( Apenas regras de contratos, sem logica  )
+        {
+            DateTime GetDateTime { get; set; }
+            String GetZipcode { get; set; }
+            void GetPaymentDetails(double value);
+        }
+    }
+}
