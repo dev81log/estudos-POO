f6a5265b59d41f5011cccbb42afb150ccf91e87d
dev81log <dev81log@gmail.com>
2022-06-17 17:28:33 -0300

    Delegates

---
+++ Delegates/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var pagar = new Pagamento.Pagar(RealizarPagamento); // instanciando pagar pelo delegate da class Pagamento
+        pagar(44); // apagar agora recebe o double injetar em RealizarPagamento
+    }
+
+    static void RealizarPagamento(double value) // Metdodo que realiza o pagamento
+    {
+        Console.WriteLine($"Pagamento realizado: {value}");
+    }
+
+    class Pagamento
+    {
+        public delegate void Pagar(double value); // Delegate aponta para a metodo que receberá um double
+                                                  // Assinaturas iguais
+    }
+}
