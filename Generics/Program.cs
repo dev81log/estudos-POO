06ae1c49845b6740d1983e4106c2d3a72b29ab00
dev81log <dev81log@gmail.com>
2022-06-22 12:15:38 -0300

    Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var payment = new Payment();
+            var person = new Person();
+            var subscription = new Subscription();
+            var context = new DataContext<Payment, Person, Subscription>();
+            context.Save(payment);
+            context.Save(person);
+            context.Save(subscription);
+        }
+    }
+
+    public class DataContext<P, PP, S>
+        where P : Payment
+        where PP : Person
+        where S : Subscription
+        // antes da chave
+    {
+
+        public void Save(P entity)
+        {
+
+        }
+
+        public void Save(PP entity)
+        {
+
+        }
+
+        public void Save(S entity)
+        {
+
+        }
+    }
+
+    public class Payment { }
+
+    public class Person { }
+
+    public class Subscription { }
+}
